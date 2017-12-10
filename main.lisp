57c4c9a2c789f6f356b5e84ac78d56cd644fa9b7
Adrian Lizarraga <adrian.lizarraga@zybooks.com>
2017-12-09 22:05:03 -0800

    Replace append with nconc

--- main.lisp
+++ main.lisp
@@ -1,1 +1,1 @@
-(defun o(a b)(loop for r in b append(remove-if #'(lambda(i)(or(< i (nth 0 r))(> i (nth 1 r))))a)))
+(defun o(a b)(loop for r in b nconc(remove-if #'(lambda(i)(or(< i (nth 0 r))(> i (nth 1 r))))a)))
